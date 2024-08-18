e82864c00c1d29a6c267eaf6e6ac1387686a6f44
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-08-19 00:53:15 +0300

    Update arch.cpp

--- data/system.v/arch.cpp
+++ data/system.v/arch.cpp
@@ -7,12 +7,9 @@ class AutonomousCar {
 public:
 
     void navigateToDestination(int destination) {
-        // Assuming simple linear navigation for demonstration
         for (int i = 0; i < destination; ++i) {
-            // Perform navigation actions
-            std::cout << "Moving to waypoint " << i + 1 << std::endl;
-            // Simulate some computation
-            simulateComputation();
+           std::cout << "Moving to waypoint " << i + 1 << std::endl;
+                  simulateComputation();
         }
         std::cout << "Destination reached!" << std::endl;
     }
@@ -20,8 +17,6 @@ public:
 private:
    
     void simulateComputation() {
-        // This is just a placeholder for computational complexity
-        // In a real system, this function would perform actual computations
         for (int i = 0; i < 1000000; ++i) {
             // Simulate computation
         }
